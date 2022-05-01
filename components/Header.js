c26b45c79d57e8d3cb545b45704db9608fa2a485
haesoo <wjdgotn77@gmail.com>
2022-05-01 22:56:39 +0900

    Modify: Header 스타일 수정

--- components/Header.js
+++ components/Header.js
@@ -14,18 +14,11 @@ export default function Header() {
     <Box sx={{ flexGrow: 1 }}>
       <StyledAppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            믿고 보는 맛집 리스트
-          </Typography>
+          <Link href={"/"} passHref>
+            <StyledTypography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              믿고 보는 맛집 리스트
+            </StyledTypography>
+          </Link>
           <Link href={"/about"} passHref>
             <Button color="inherit">About</Button>
           </Link>
@@ -37,4 +30,8 @@ export default function Header() {
 
 const StyledAppBar = styled(AppBar)`
   background: ${({ theme }) => theme.dark};
+`;
+
+const StyledTypography = styled(Typography)`
+  cursor: pointer;
 `;
