7e293ab440f4a470d3445017cd6e5dcd335492d4
haesoo <wjdgotn77@gmail.com>
2022-05-02 00:30:56 +0900

    Add: HomePage 맛집리스트 추가.
    index.js
    - local 통신코드 추가.
    - wip - 리스트로 스타일 수정.

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,49 @@
 import Head from "next/head";
+// import Image from "next/image";
+import styled from "styled-components";
 
-export default function Home() {
+export default function Home({ posts }) {
+  console.log(posts);
   return (
     <div>
       <Head>
         <title>HOME | AWESOME FOOD STORE</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
+      <Container>
+        {posts.map((post) => (
+          <ImageWrapper key={post.id}>
+            {/* next/image로 수정. */}
+            <Img alt={post.name} src={post.thumb} />
+            <span>{post.name}</span>
+          </ImageWrapper>
+        ))}
+      </Container>
     </div>
   );
 }
+
+export async function getStaticProps() {
+  const res = await fetch("http://localhost:9000/stores");
+  const posts = await res.json();
+
+  return {
+    props: { posts },
+  };
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  padding: 40px 80px;
+`;
+
+const ImageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 10px;
+`;
+
+const Img = styled.img`
+  width: 300px;
+`;
